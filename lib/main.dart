0546b941d9eafe17ac7a61c80a08d711eb050ff2
Kihyun Kim <hyunkim0306@gmail.com>
2024-09-24 18:12:57 -0400

    Changed title to 'digital_pet_app'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'digital_pet_app',
       theme: ThemeData(
         // This is the theme of your application.
         //
